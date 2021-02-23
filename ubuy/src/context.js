406f3dde907094d1faf2345a6a6bc2ebf7fda607
Anu <anoosathyee@gmail.com>
2021-02-23 02:27:28 -0600

    added modal.js

--- ubuy/src/context.js
+++ ubuy/src/context.js
@@ -10,6 +10,8 @@ class ProductProvider extends Component {
     products: [],
     detailProduct: detailProduct,
     cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct,
   };
 
   componentDidMount() {
@@ -55,6 +57,18 @@ class ProductProvider extends Component {
     );
   };
 
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
+
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
+  };
   render() {
     return (
       <ProductContext.Provider
@@ -62,6 +76,8 @@ class ProductProvider extends Component {
           ...this.state,
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
+          openModal: this.openModal,
+          closeModal: this.closeModal,
         }}
       >
         {this.props.children}
