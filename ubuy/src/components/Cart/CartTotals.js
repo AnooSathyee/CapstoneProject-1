af7d2c5f86b5824162c27fb206de861ceb892fb6
Anu <anoosathyee@gmail.com>
2021-02-23 13:12:50 -0600

    adding some final components to my cart

--- ubuy/src/components/Cart/CartTotals.js
+++ ubuy/src/components/Cart/CartTotals.js
@@ -22,12 +22,12 @@ export default function CartTotals({ value }) {
               </h5>
 
               <h5>
-                <span className="text-title">subtotal:</span>
+                <span className="text-title">Tax:</span>
                 <strong>${cartTax}</strong>
               </h5>
 
               <h5>
-                <span className="text-title">subtotal:</span>
+                <span className="text-title">Total:</span>
                 <strong>${cartTotal}</strong>
               </h5>
             </Link>
--- ubuy/src/context.js
+++ ubuy/src/context.js
@@ -82,11 +82,52 @@ class ProductProvider extends Component {
   };
 
   removeItem = (id) => {
-    console.log("item removed");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter((item) => item.id !== id);
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index]; //the exact product
+    removedProduct.inCart = false;
+    removedProduct.count = 0; //since removing it set back to 0
+    removedProduct.total = 0;
+
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts],
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   clearCart = () => {
-    console.log("cart was cleared");
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      }
+    );
+  };
+
+  clearCart = () => {
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      }
+    );
   };
 
   addTotals = () => {
