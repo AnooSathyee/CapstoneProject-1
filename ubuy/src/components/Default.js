dbd2256b32d59dd4c9ae8aa49ce4c718fe6dda39
Anu <anoosathyee@gmail.com>
2021-02-23 13:26:03 -0600

    Done

--- ubuy/src/components/Default.js
+++ ubuy/src/components/Default.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 export default class Default extends Component {
   render() {
     return (
-      <div>
+      <div align="center">
+        <h1>404 Error</h1>
         <h3>Page Not found!</h3>
       </div>
     );
