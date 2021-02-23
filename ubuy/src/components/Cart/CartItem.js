5ef00ab06a59009bad3ec0716f1bd284e362cdaa
Anu <anoosathyee@gmail.com>
2021-02-23 10:45:14 -0600

    added remove button icon, and quantity increment and decrement icons

--- ubuy/src/components/Cart/CartItem.js
+++ ubuy/src/components/Cart/CartItem.js
@@ -16,13 +16,39 @@ export default function CartItem({ item, value }) {
       </div>
 
       <div className="col-10 mx-auto col-lg-2">
-        <span className="d-lg-none">product : </span>
+        <span className="d-lg-none">product:</span>
         {title}
       </div>
 
       <div className="col-10 mx-auto col-lg-2">
-        <span className="d-lg-none">price : </span>
+        <span className="d-lg-none">price :</span>
         {price}
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <span className="btn btn-black mx-1" onClick={() => decrement(id)}>
+              {" "}
+              -
+            </span>
+            <span className="btn btn-black mx-1">{count}</span>
+            <span className="btn btn-black mx-1" onClick={() => increment(id)}>
+              {" "}
+              +
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <div className="cart-icon" onClick={() => removeItem(id)}>
+          <i className="fas fa-trash" />
+        </div>
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <strong>item total: $ {total}</strong>
       </div>
     </div>
   );
